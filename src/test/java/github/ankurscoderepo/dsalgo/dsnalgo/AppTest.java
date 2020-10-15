98c65c9c47bb2bf4f7e75ff7be14c6401c71e641
Administrator <Administrator@172.31.43.114>
2020-10-15 06:06:15 +0000

    Converted to maven project

---
+++ src/test/java/github/ankurscoderepo/dsalgo/dsnalgo/AppTest.java
@@ -0,0 +1,14 @@
+package github.ankurscoderepo.dsalgo.dsnalgo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+
+	}
+
+}
+
